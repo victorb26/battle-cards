e4de6386acac3ff712042b68ffab9a1f04631dc4
Victor Barros <victorjuda49@gmail.com>
2022-08-08 15:21:59 -0300

    criando schema

---
+++ src/models/cards.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const cardSchema = new mongoose.Schema ({
+    _id: {type: Number, required: true},
+    name:{type: String, required: true},
+    attributes: {
+        hp:{type: Number, required: true},
+        attack:{type: Number, required: true},
+        defense: {type: Number, required: true},
+        speed:{type: Number, required: true},
+        specialAttack:{type: Number, required: true},
+        specialDefense:{type: Number, required: true},
+        _id:false
+    }, required: true,},
+
+
+)
