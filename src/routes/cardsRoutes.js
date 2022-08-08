fafe33ac6fd06b0fab7b33dadf29a0f2b754a038
Victor Barros <victorjuda49@gmail.com>
2022-08-08 16:18:51 -0300

    finalizando detalhes das rotas e suas importações/exportações

--- src/routes/cardsRoutes.js
+++ src/routes/cardsRoutes.js
@@ -1,10 +1,10 @@
-import express, { Router } from "express";
+import express from "express";
 
 const router = express.Router();
 
 router.get("/pokemons");
 router.post("/pokemon");
-router.put("/pokemon/:id");
+router.patch("/pokemon/:id");
 router.delete("/pokemon/:_id");
 
 export default router;
