d670abfa28152e54478d272f94c879a8a0fc5d02
Victor Barros <victorjuda49@gmail.com>
2022-08-10 14:37:08 -0300

    fix: atualizando organização dos códigos

--- src/controllers/cardController.js
+++ src/controllers/cardController.js
@@ -3,30 +3,30 @@ import cardModel from "../models/Cards.js";
 export default class CardController {
   static getAll = (req, res) => {
     cardModel.find((err, cardModel) => {
-      cardModel.sort((a, b) => a._id - b._id);
-      res.status(200).json(cardModel);
+      if (err) {
+        res.status(500).json({ message: `Internal Server Error` });
+      } else {
+        res.status(200).json(cardModel);
+      }
     });
   };
 
   static getId = (req, res) => {
-        const id = req.params.id;
-    
-        cardModel.findById(id, (err, cardModel) => {
-          if(err) {
-            res.status(404).json({message: `ID not found`})
-          } else {
-            res.status(200).json(cardModel);
-          }
-        })
+    let id = req.params.id;
+    cardModel.findById(id, (err, cardModel) => {
+      if (err) {
+        res.status(400).json({ message: `Error :()` });
+      } else {
+        res.status(200).json(cardModel);
       }
+    });
+  };
 
   static insert = (req, res) => {
     const character = new cardModel(req.body);
     character.save((err) => {
       if (err) {
-        res.status(404).json({
-          message: `Error! Insert all params!/Character already registered `,
-        });
+        res.status(404).json({ message: `Error! Insert all params!/Character already registered `});
       } else {
         res.status(201).json({ message: `Sucess! Character has been added!` });
       }
@@ -36,26 +36,21 @@ export default class CardController {
   static update = (req, res) => {
     const id = req.params.id;
     cardModel.findByIdAndUpdate(id, { $set: req.body }, (err) => {
-      if (!err) {
-        res.status(201).json({ message: `Character updated successfully` });
+      if (err) {
+        res.status(404).json({ message: `Something's wrong!/Character notFound` });
       } else {
-        res
-          .status(404)
-          .json({ message: `Something's wrong!/Character notFound` });
+        res.status(201).json({ message: `Character updated successfully` });
       }
     });
   };
 
   static delete = (req, res) => {
     const id = req.params.id;
-
     cardModel.findByIdAndDelete(id, (err) => {
-      if (!err) {
-        res.status(201).json({ message: `Character removed sucessfully!` });
+      if (err) {
+        res.status(404).json({ message: `Something's wrong!/Could not delete character` });
       } else {
-        res
-          .status(404)
-          .json({ message: `Something's wrong!/Could not delete character` });
+        res.status(201).json({ message: `Character removed sucessfully!` });
       }
     });
   };
