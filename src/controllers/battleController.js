2548f3ae4e1df0180d6a75cc99c85317f582c1d8
Victor Barros <victorjuda49@gmail.com>
2022-08-14 20:57:36 -0300

    feat: controle das batalhas

--- src/controllers/battleController.js
+++ src/controllers/battleController.js
@@ -17,12 +17,49 @@ export default class BattleController {
     const characStatus2 = result2.attributes.toObject();
 
     let resultHp = "";
+    let resultAttack = "";
+    let resultDefense = "";
+    let resultSpeed = "";
+    let resultSpecialAttack = "";
+    let resultSpecialDefense = "";
+
+
 
     if (characStatus1.hp > characStatus2.hp) {
       resultHp = result1.toObject()._id;
     } else {
       resultHp = result2.toObject()._id;
-    }
+    };
+
+    if (characStatus1.attack > characStatus2.attack){
+      resultAttack = result1.toObject()._id
+    }else{
+      resultAttack = result2.toObject()._id
+    };
+
+    if (characStatus1.defense > characStatus2.defense) {
+      resultDefense = result1.toObject()._id
+    }else{
+      resultDefense = result2.toObject()._id
+    };
+
+    if(characStatus1.speed > characStatus2.speed){
+      resultSpeed = result1.toObject()._id
+    }else{
+      resultSpeed = result2.toObject()._id
+    };
+
+    if (characStatus1.specialAttack > characStatus2.specialAttack) {
+      resultSpecialAttack = result1.toObject()._id
+    }else{
+      resultSpecialAttack = result2.toObject()._id
+    };
+
+    if (characStatus1.specialDefense > characStatus2.specialDefense) {
+      resultSpecialDefense = result1.toObject()._id
+    }else{
+      resultSpecialDefense = result2.toObject()._id
+    };
 
 
     const power = sum(characStatus1, characStatus2);
@@ -31,7 +68,12 @@ export default class BattleController {
       power.charac2,
       result1,
       result2,
-      resultHp
+      resultHp,
+      resultAttack,
+      resultDefense,
+      resultSpeed,
+      resultSpecialAttack,
+      resultSpecialDefense
     );
 
     res.status(200).send({ FinalResult });
--- src/models/compare.js
+++ src/models/compare.js
@@ -1,7 +1,40 @@
-const compare = (object1, object2, result1, result2, resultHp) => {
+const compare = (
+  object1,
+  object2,
+  result1,
+  result2,
+  resultHp,
+  resultAttack,
+  resultDefense,
+  resultSpeed,
+  resultSpecialAttack,
+  resultSpecialDefense
+) => {
   return object1 > object2
-    ? { winner: result1.id, loser: result2.id, details:{hp:resultHp} }
-    : { winner: result2.id, loser: result1.id, details:{hp:resultHp} };
+    ? {
+        winner: result1.id,
+        loser: result2.id,
+        details: {
+          hp: resultHp,
+          attack: resultAttack,
+          defense: resultDefense,
+          speed: resultSpeed,
+          specialAttack: resultSpecialAttack,
+          specialDefense: resultSpecialDefense,
+        },
+      }
+    : {
+        winner: result2.id,
+        loser: result1.id,
+        details: {
+          hp: resultHp,
+          attack: resultAttack,
+          defense: resultDefense,
+          speed: resultSpeed,
+          specialAttack: resultSpecialAttack,
+          specialDefense: resultSpecialDefense,
+        },
+      };
 };
 
 export default compare;
